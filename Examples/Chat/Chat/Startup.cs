bc79e18eae3a36dbd9a56ee55abe78504b0e9625
Luis Ruiz Pavón <lruiz@plainconcepts.com>
2018-07-04 08:27:28 +0200

    Add chat example

---
+++ Examples/Chat/Chat/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Chat.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chat
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseSignalR(routes => routes.MapHub<ChatHub>("/chat"));
+        }
+    }
+}
